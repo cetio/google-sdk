c2fc283e762f2ed9f9a2193e001bbaee3021260d
cetio <stakeandtake39@gmail.com>
2026-06-05 18:53:13 -0400

    Add Gmail package manifest

---
+++ source/google/gmail/package.d
@@ -0,0 +1,10 @@
+module google.gmail;
+
+public:
+
+public import google.gmail.error;
+public import google.gmail.identity;
+public import google.gmail.label;
+public import google.gmail.message;
+public import google.gmail.session;
+public import google.gmail.thread;
